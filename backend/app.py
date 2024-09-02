11eca1472e041e82d5e84696bc6ea00dbf1963e4
Jose Samuel Berigüete Valero <aislen404@gmail.com>
2024-09-02 17:30:25 +0200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,25 +1,14 @@
-from flask import Flask, jsonify, request
-from models import db, Widget
-from flask_migrate import Migrate
+from flask import Flask
+from models import db
+from routes import api  # Importar el Blueprint
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
-migrate = Migrate(app, db)
 
-@app.route('/api/widgets', methods=['GET'])
-def get_widgets():
-    widgets = Widget.query.all()
-    return jsonify([widget.to_dict() for widget in widgets])
-
-@app.route('/api/widgets', methods=['POST'])
-def create_widget():
-    data = request.json
-    new_widget = Widget(title=data['title'], style=data['style'], data=data['data'])
-    db.session.add(new_widget)
-    db.session.commit()
-    return jsonify(new_widget.to_dict()), 201
+# Registrar el Blueprint
+app.register_blueprint(api)
 
 if __name__ == '__main__':
     app.run(debug=True)
